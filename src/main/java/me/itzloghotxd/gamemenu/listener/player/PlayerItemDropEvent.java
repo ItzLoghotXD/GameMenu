b95b0a8b47f997d83fcd8a3c964751158e438275
ItzLoghotXD <loghot.gamerz.official@gmail.com>
2024-12-07 20:40:32 +0530

    Updated custom hotbar item and changed version

--- src/main/java/me/itzloghotxd/gamemenu/listener/player/PlayerItemDropEvent.java
+++ src/main/java/me/itzloghotxd/gamemenu/listener/player/PlayerItemDropEvent.java
@@ -1,7 +1,10 @@
 package me.itzloghotxd.gamemenu.listener.player;
 
+import me.itzloghotxd.gamemenu.GamemenuPlugin;
+import me.itzloghotxd.gamemenu.config.ConfigType;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,13 +12,18 @@ import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerItemDropEvent implements Listener {
+
+    private String item;
+
     public PlayerItemDropEvent() {
     }
 
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent event) {
+        FileConfiguration config = GamemenuPlugin.getPlugin().getConfigManager().getConfig(ConfigType.SETTINGS);
         ItemStack itemStack = event.getItemDrop().getItemStack();
         Player player = event.getPlayer();
+        item = config.getString("server_selector.item");
         if (itemStack.getType() == Material.NETHER_STAR) {
             event.setCancelled(true);
             player.sendMessage(ChatColor.RED + "You can't drop this item");
--- src/main/java/me/itzloghotxd/gamemenu/listener/player/PlayerOffHandSwapEvent.java
+++ src/main/java/me/itzloghotxd/gamemenu/listener/player/PlayerOffHandSwapEvent.java
@@ -1,21 +1,28 @@
 package me.itzloghotxd.gamemenu.listener.player;
 
+import me.itzloghotxd.gamemenu.GamemenuPlugin;
+import me.itzloghotxd.gamemenu.config.ConfigType;
 import org.bukkit.Material;
 //import org.bukkit.entity.Player;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerOffHandSwapEvent implements Listener {
+
+    private String item;
+
     public PlayerOffHandSwapEvent() {
     }
 
     @EventHandler
     public void onPlayerOffHandSwap(PlayerSwapHandItemsEvent event) {
+        FileConfiguration config = GamemenuPlugin.getPlugin().getConfigManager().getConfig(ConfigType.SETTINGS);
         ItemStack itemStack = event.getOffHandItem();
-//        Player player = event.getPlayer();
-        if (itemStack.getType() == Material.NETHER_STAR) {
+        item = config.getString("server_selector.item");
+        if (itemStack.getType() == Material.getMaterial(item)) {
             event.setCancelled(true);
         }
     }

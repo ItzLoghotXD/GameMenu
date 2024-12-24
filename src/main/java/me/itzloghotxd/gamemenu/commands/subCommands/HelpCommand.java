60a79befb8e7ea9ab6e38cbfc76d8bfa0c3c5395
ItzLoghotXD <loghot.gamerz.official@gmail.com>
2024-12-24 16:46:33 +0530

    updated help command

--- src/main/java/me/itzloghotxd/gamemenu/commands/subCommands/HelpCommand.java
+++ src/main/java/me/itzloghotxd/gamemenu/commands/subCommands/HelpCommand.java
@@ -1,6 +1,7 @@
 package me.itzloghotxd.gamemenu.commands.subCommands;
 
 import me.itzloghotxd.gamemenu.commands.Command;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 import java.util.List;
@@ -8,7 +9,12 @@ import java.util.List;
 public class HelpCommand implements Command {
     @Override
     public void execute(CommandSender sender, String[] args) {
-        // I will add this later cuz I am very lazy :)
+        sender.sendMessage(ChatColor.AQUA + "--- GameMenu Plugin Help ---");
+        sender.sendMessage(ChatColor.GOLD + "/gm" + ChatColor.WHITE + " - Main command");
+        sender.sendMessage(ChatColor.GOLD + "/gm help" + ChatColor.WHITE + " - Display this help message");
+        sender.sendMessage(ChatColor.GOLD + "/gm reload" + ChatColor.WHITE + " - Reloads the plugin configuration");
+        sender.sendMessage(ChatColor.GOLD + "/gm menu" + ChatColor.WHITE + " - Opens the default/main menu");
+        sender.sendMessage(ChatColor.AQUA + "------------------------------");
     }
 
     @Override
